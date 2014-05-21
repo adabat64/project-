5a7463f8956a94dedd5a310def57b0b8472fbe86
Quinn Rohlf <qr@qrohlf.com>
2014-05-21 08:10:06 -0700

    more examples

--- app.rb
+++ app.rb
@@ -11,3 +11,8 @@ Bundler.require
 get '/' do
 	erb :index
 end
+
+get '/99bottles' do
+  @lyrics = (1..99).to_a.reverse.map {|i| "#{i} bottles of beer on the wall, #{i} bottles of beer. Take one down, pass it around, #{i-1} bottles of beer on the wall."}
+  erb :bottles
+end
